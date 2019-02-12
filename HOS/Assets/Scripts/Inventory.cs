4f0194d041e8d7a50b487b76e6b7cae35aacaa4a
jjmasters <47305282+jjmasters@users.noreply.github.com>
2019-02-12 17:37:23 -0500

    Working on Inventory

--- HOS/Assets/Scripts/Inventory.cs
+++ HOS/Assets/Scripts/Inventory.cs
@@ -6,16 +6,19 @@ using UnityEngine.UI;
 public class Inventory : MonoBehaviour 
 {
     public Dictionary<InventoryItem, Item> ItemsInInventory = new Dictionary<InventoryItem,Item>();
-    
+    public RawImage ThisPicture;
 	// Use this for initialization
-	void Start () {
-		
+	void Start () 
+    {
+        Item NewItem = new Item();
+        NewItem.ItemImage = ThisPicture;
+        ItemsInInventory.Add(InventoryItem.Trowel,NewItem);
 	}
 	
 	// Update is called once per frame
 	void Update () 
     {
-		
+
 	}
 
     public void AddInventoryItem()
