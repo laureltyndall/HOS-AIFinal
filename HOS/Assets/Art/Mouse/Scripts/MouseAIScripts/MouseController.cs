a35c618df037304100785c2bfcc65568fbf39263
Schamale <38950754+Schamale@users.noreply.github.com>
2019-02-13 01:37:09 -0500

    Mouse AI
    Mouse AI Agent

---
+++ HOS/Assets/Art/Mouse/Scripts/MouseAIScripts/MouseController.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+using HOS;
+
+public class MouseController : MonoBehaviour {
+
+    public Camera cam;
+    public NavMeshAgent agent;
+
+	// Update is called once per frame
+	void Update ()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            Ray r = cam.ScreenPointToRay(Input.mousePosition);
+            RaycastHit hit;
+
+            if (Physics.Raycast(r, out hit))
+            {
+                //Move Our Mouse Agent
+                agent.SetDestination(hit.point);
+            }
+        }
+		
+	}
+}
