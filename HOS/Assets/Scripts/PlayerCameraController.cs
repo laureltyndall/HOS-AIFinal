d7e45e085ce5022a6a510f2ef916e755918b4a6d
jjmasters <47305282+jjmasters@users.noreply.github.com>
2019-02-18 15:56:56 -0500

    Created PlayerCameraController Class
    Very basic setup. Working on controls to prevent oversteering. May need further tweaking.

---
+++ HOS/Assets/Scripts/PlayerCameraController.cs
@@ -0,0 +1,67 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerCameraController : MonoBehaviour 
+{
+    public bool CanOrbit = false;
+    private int TurnsLeft = 0;
+    private int TurnsRight = 0;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+    public void MovePlayerFoward()
+    {
+        this.gameObject.transform.Translate(0,0,10);
+    }
+
+    public void MovePlayerBackward()
+    {
+        this.gameObject.transform.Translate(0,0,-10);
+    }
+
+    public void MovePlayerLeft()
+    {
+        this.gameObject.transform.Translate(-10,0,0);
+    }
+
+    public void MovePlayerRight()
+    {
+        this.gameObject.transform.Translate(10,0,0);
+    }
+
+    public void MovePlayerUturn()
+    {
+        this.gameObject.transform.Translate(0,0,0);
+        this.gameObject.transform.Rotate(0,180,0);
+    }
+
+    public void OrbitLeft()
+    {
+        if (CanOrbit)
+        {
+            if (TurnsLeft <= 0)
+            {
+                this.gameObject.transform.GetChild(0).transform.Translate(-4, 0, 0);
+            }
+            this.gameObject.transform.Rotate(0, -90, 0);
+            TurnsLeft += 1;
+        }
+    }
+
+    public void OrbitRight()
+    {
+        if (CanOrbit)
+        {
+            if (TurnsRight <= 0)
+            {
+                this.gameObject.transform.GetChild(0).transform.Translate(4, 0, 0);
+            }
+            this.gameObject.transform.Rotate(0, -90, 0);
+            TurnsRight += 1;
+        }
+    }
+}
