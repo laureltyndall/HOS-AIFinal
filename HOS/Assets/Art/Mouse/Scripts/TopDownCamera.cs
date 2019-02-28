48131e6592256b72a8f1f4e6f0e64b9903fd4018
Laurel Tyndall <laureltyndall@yahoo.com>
2019-02-27 21:24:51 -0500

    Backup Implemented

---
+++ HOS/Assets/Art/Mouse/Scripts/TopDownCamera.cs
@@ -0,0 +1,67 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using HOS;
+
+public class TopDownCamera : MonoBehaviour
+{
+    #region Variables
+    public Transform m_target;      //my target for camera
+    [SerializeField]
+    private float m_Height = 3f;    //my height for camera
+    [SerializeField]
+    private float m_Distance = 0f;  //my distance for camera
+    [SerializeField]
+    private float m_Angle = 1f;     //my angle for camera
+    [SerializeField]
+    private float m_Smoothspeed = 0.5f;     //Smoothing the camera speed
+    [SerializeField]
+    private float boundX = 2.0f;
+    [SerializeField]
+    private float boundY = 1.5f;
+
+    private Vector3 refVelocity;
+    #endregion
+
+    #region Default Constructors
+    // Use this for initialization
+    void Start ()
+    {
+        HandleCamera();
+		
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        HandleCamera();
+		
+	}
+    #endregion
+
+    protected virtual void HandleCamera()
+    {
+        if (!m_target)
+        {
+            return;
+        }
+
+        //World Position Vector
+        Vector3 worldPosition = (Vector3.forward * -m_Distance) + (Vector3.up * m_Height);
+        Debug.DrawLine(m_target.position, worldPosition, Color.red);
+
+        //Build Rotation Vector
+        Vector3 rotatedVector = Quaternion.AngleAxis(m_Angle, Vector3.up) * worldPosition;
+        Debug.DrawLine(m_target.position, rotatedVector, Color.green);
+
+        //Move Camera Position
+        Vector3 flatTargetPosition = m_target.position;
+        flatTargetPosition.y = 0f;
+        Vector3 finalPosition = flatTargetPosition + rotatedVector;
+        Debug.DrawLine(m_target.position, finalPosition, Color.blue);
+
+        transform.position = Vector3.SmoothDamp(transform.position, finalPosition, ref refVelocity, m_Smoothspeed);
+        //transform.position = finalPosition;
+        transform.LookAt(m_target.position);
+    }
+}
--- HOS/Assets/Scripts/PlayerCameraController.cs
+++ HOS/Assets/Scripts/PlayerCameraController.cs
@@ -3,11 +3,15 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.EventSystems;
 
-public class PlayerCameraController : MonoBehaviour, IPointerClickHandler, IPointerEnterHandler
+public class PlayerCameraController : MonoBehaviour, IPointerClickHandler, IPointerEnterHandler, IPointerExitHandler
 {
     public Player CurrentPlayer;
     public bool CanOrbit = false;
+    public bool CanBackup = false;
+    public float RotateTime = 0.2f;
     public List<GameObject> WaypointList = new List<GameObject>();
+    public List<Texture2D> CursorList;
+    CursorType CurrentCursor = CursorType.Default;
 
 	// Use this for initialization
 	void Start () {
@@ -22,30 +26,79 @@ public class PlayerCameraController : MonoBehaviour, IPointerClickHandler, IPoin
 
     public void OnPointerClick(PointerEventData data)
     {
-        if ((data.pressPosition.x >= 0 && data.pressPosition.x <= 200) && (data.pressPosition.y >= 0 && data.pressPosition.y <= 451))
+        if (CurrentCursor == CursorType.Forward)
         {
             CurrentPlayer.transform.position = WaypointList[0].transform.position;
         }
-        if ((data.pressPosition.x >= 0 && data.pressPosition.x <= 200) && (data.pressPosition.y >= 0 && data.pressPosition.y <= 451))
+        if (CurrentCursor == CursorType.TurnAround)
         {
-            CurrentPlayer.transform.position = WaypointList[0].transform.position;
+            MovePlayerUturn();
         }
-        
     }
 
     public void OnPointerEnter(PointerEventData data)
     {
+        Debug.Log("Position X: " + Input.mousePosition.x.ToString() + " , Position Y: " + Input.mousePosition.y.ToString());
+
         if (CanOrbit)
         {
-            if ((data.pressPosition.x >= 0 && data.pressPosition.x <= 450) && (data.pressPosition.y >= 0 && data.pressPosition.y <= 800))
+            if ((Input.mousePosition.x >= 0 && Input.mousePosition.x <= 100) && (Input.mousePosition.y >= 0 && Input.mousePosition.y <= 800))
             {
+                Debug.Log("Rotating Left");
+                Cursor.SetCursor(CursorList[4], Vector2.zero, CursorMode.Auto);
+                CurrentCursor = CursorType.Panoramic;
                 RotateLeft();
             }
-            if ((data.pressPosition.x >= 900 && data.pressPosition.x <= 1400) && (data.pressPosition.y >= 0 && data.pressPosition.y <= 800))
+            if ((Input.mousePosition.x >= 1100 && Input.mousePosition.x <= 1400) && (Input.mousePosition.y >= 0 && Input.mousePosition.y <= 800))
             {
+                Debug.Log("Rotating Right");
+                Cursor.SetCursor(CursorList[4], Vector2.zero, CursorMode.Auto);
+                CurrentCursor = CursorType.Panoramic;
                 RotateRight();
             }
         }
+        else
+        {
+            if ((Input.mousePosition.x >= 0 && Input.mousePosition.x <= 100) && (Input.mousePosition.y >= 0 && Input.mousePosition.y <= 800))
+            {
+                Debug.Log("Changing to Turn Left Cursor");
+                Cursor.SetCursor(CursorList[6], Vector2.zero, CursorMode.Auto);
+                CurrentCursor = CursorType.LeftTurn;
+            }
+            if ((Input.mousePosition.x >= 1100 && Input.mousePosition.x <= 1400) && (Input.mousePosition.y >= 0 && Input.mousePosition.y <= 800))
+            {
+                Debug.Log("Changing to Turn Right Cursor");
+                Cursor.SetCursor(CursorList[7], Vector2.zero, CursorMode.Auto);
+                CurrentCursor = CursorType.RightTurn;
+            }
+            if ((Input.mousePosition.x >= 539 && Input.mousePosition.x <= 936) && (Input.mousePosition.y >= 234 && Input.mousePosition.y <= 800))
+            {
+                Debug.Log("Changing to Forward Cursor");
+                Cursor.SetCursor(CursorList[3], Vector2.zero, CursorMode.Auto);
+                CurrentCursor = CursorType.Forward;
+            }
+            if ((Input.mousePosition.x >= 100 && Input.mousePosition.x <= 1100) && (Input.mousePosition.y >= 50 && Input.mousePosition.y <= 120))
+            {
+                if (CanBackup)
+                {
+                    Debug.Log("Changing to Backup Cursor");
+                    Cursor.SetCursor(CursorList[0], Vector2.zero, CursorMode.Auto);
+                    CurrentCursor = CursorType.Backup;
+                }
+                else
+                {
+                    Debug.Log("Changing to U-Turn Cursor");
+                    Cursor.SetCursor(CursorList[8], Vector2.zero, CursorMode.Auto);
+                    CurrentCursor = CursorType.TurnAround;
+                }
+            }
+        }
+    }
+
+    public void OnPointerExit(PointerEventData data)
+    {
+       Cursor.SetCursor(null, Vector2.zero, CursorMode.Auto);
+        CurrentCursor = CursorType.Default;
     }
 
     public void MovePlayerUturn()
@@ -61,5 +114,10 @@ public class PlayerCameraController : MonoBehaviour, IPointerClickHandler, IPoin
     public void RotateRight()
     {
         CurrentPlayer.transform.Rotate(0,90,0);
+    }
+
+    public void OnGUI()
+    {
+      //  GUI.Box(new Rect(0, 0, 4, 3), );
     }
 }
