58461973cc44b5cc0e5e869af2b632cdf0227919
Laurel Tyndall <laureltyndall@yahoo.com>
2019-03-02 16:43:40 -0500

    Begun Work on Slider Puzzle

--- HOS/Assets/Scripts/CursorChanger.cs
+++ HOS/Assets/Scripts/CursorChanger.cs
@@ -4,12 +4,24 @@ using UnityEngine;
 
 public class CursorChanger : MonoBehaviour {
 
-  //  public Texture2D NewCursor;
+    public Texture2D NewCursor;
 
     public void Start()
     {
         
     }
 
+    void OnMouseOver()
+    {
+        //If your mouse hovers over the GameObject with the script attached, output this message
+        Debug.Log("Mouse is over Letter.");
+        Cursor.SetCursor(NewCursor, Vector2.zero, CursorMode.Auto);
+    }
 
+    void OnMouseExit()
+    {
+        //The mouse is no longer hovering over the GameObject so output this message each frame
+        Debug.Log("Mouse is no longer on Letter.");
+        Cursor.SetCursor(null, Vector2.zero, CursorMode.Auto);
+    }
 }
---
+++ HOS/Assets/Scripts/MoveBlockPiece.cs
@@ -0,0 +1,57 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MoveBlockPiece : MonoBehaviour {
+
+    public Texture2D LeftCursor;
+    public Texture2D RightCursor;
+    public Texture2D UpCursor;
+    public Texture2D DownCursor;
+
+    public enum CanMove
+    {
+        None = 0,
+        Left = 1,
+        Right = 2
+    }
+
+    public CanMove CanOnlyMove = 0;
+
+    private void Start()
+    {
+        
+    }
+
+    private void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            if(this.tag == "HorizontalPiece")
+
+            {
+
+            }
+        }
+    }
+
+    void OnMouseEnter()
+    {
+        //If your mouse hovers over the GameObject with the script attached, output this message
+        Debug.Log("Mouse is over: " + this.name);
+        Debug.Log("Position X: " + Input.mousePosition.x.ToString() + " , Position Y: " + Input.mousePosition.y.ToString());
+
+    }
+
+    void OnMouseOver()
+    {
+
+    }
+
+    void OnMouseExit()
+    {
+        //The mouse is no longer hovering over the GameObject so output this message each frame
+        Debug.Log("Mouse is no longer on Block.");
+        Cursor.SetCursor(null, Vector2.zero, CursorMode.Auto);
+    }
+}
