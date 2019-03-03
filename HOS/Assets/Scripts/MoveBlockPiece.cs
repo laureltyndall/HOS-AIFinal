45d092b0b697953e671b7156f5dfde8d91365a13
Laurel Tyndall <laureltyndall@yahoo.com>
2019-03-02 22:17:19 -0500

    Slider Puzzle Gameplay Tech Finished

--- HOS/Assets/Scripts/MoveBlockPiece.cs
+++ HOS/Assets/Scripts/MoveBlockPiece.cs
@@ -13,24 +13,41 @@ public class MoveBlockPiece : MonoBehaviour {
     {
         None = 0,
         Left = 1,
-        Right = 2
+        Right = 2,
+        Up = 3,
+        Down = 4
     }
 
-    public CanMove CanOnlyMove = 0;
+    public CanMove ThisSideMoves;
+    public CanMove Moving = CanMove.None;
+
+    public MovingPiece ParentPiece;
 
     private void Start()
     {
-        
+        GameObject go = transform.parent.gameObject;
+        ParentPiece = go.GetComponent<MovingPiece>();
     }
 
     private void Update()
     {
-        if (Input.GetMouseButtonDown(0))
+        if (Input.GetMouseButtonUp(0))
         {
-            if(this.tag == "HorizontalPiece")
-
+            if (Moving == CanMove.Right)
             {
-
+                ParentPiece.IsMovingRight = true;
+            }
+            else if (Moving == CanMove.Left)
+            {
+                ParentPiece.IsMovingLeft = true;
+            }
+            else if (Moving == CanMove.Up)
+            {
+                ParentPiece.IsMovingUp = true;
+            }
+            else if (Moving == CanMove.Down)
+            {
+                ParentPiece.IsMovingDown = true;
             }
         }
     }
@@ -38,9 +55,32 @@ public class MoveBlockPiece : MonoBehaviour {
     void OnMouseEnter()
     {
         //If your mouse hovers over the GameObject with the script attached, output this message
-        Debug.Log("Mouse is over: " + this.name);
-        Debug.Log("Position X: " + Input.mousePosition.x.ToString() + " , Position Y: " + Input.mousePosition.y.ToString());
+        Debug.Log("Mouse is over: " + transform.parent.name + " " + this.name);
 
+        if (ThisSideMoves == CanMove.Right)
+        {
+            Debug.Log("Mouse is over Move RIGHT Side.");
+            Cursor.SetCursor(RightCursor, Vector2.zero, CursorMode.Auto);
+            Moving = CanMove.Right;
+        }
+        else if (ThisSideMoves == CanMove.Left)
+        {
+           Debug.Log("Mouse is over Move LEFT Side.");
+           Cursor.SetCursor(LeftCursor, Vector2.zero, CursorMode.Auto);
+           Moving = CanMove.Left;
+        }
+        else if (ThisSideMoves == CanMove.Up)
+        {
+            Debug.Log("Mouse is over Move UP Side.");
+            Cursor.SetCursor(UpCursor, Vector2.zero, CursorMode.Auto);
+            Moving = CanMove.Up;
+        }
+        else if (ThisSideMoves == CanMove.Down)
+        {
+            Debug.Log("Mouse is over Move DOWN Side.");
+            Cursor.SetCursor(DownCursor, Vector2.zero, CursorMode.Auto);
+            Moving = CanMove.Down;
+        }
     }
 
     void OnMouseOver()
@@ -53,5 +93,6 @@ public class MoveBlockPiece : MonoBehaviour {
         //The mouse is no longer hovering over the GameObject so output this message each frame
         Debug.Log("Mouse is no longer on Block.");
         Cursor.SetCursor(null, Vector2.zero, CursorMode.Auto);
+        Moving = CanMove.None;
     }
 }
---
+++ HOS/Assets/Scripts/MovingPiece.cs
@@ -0,0 +1,59 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MovingPiece : MonoBehaviour {
+
+    public bool IsMovingRight = false;
+    public bool IsMovingLeft = false;
+    public bool IsMovingUp = false;
+    public bool IsMovingDown = false;
+
+    private float MovementSpeed = 200f;
+
+    // Use this for initialization
+    void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (IsMovingRight)
+        {
+            transform.Translate(-Vector3.right * MovementSpeed * Time.deltaTime);
+        }
+
+        if (IsMovingLeft)
+        {
+            transform.Translate(Vector3.right * MovementSpeed * Time.deltaTime);
+        }
+
+        if (IsMovingUp)
+        {
+            transform.Translate(Vector3.right * MovementSpeed * Time.deltaTime);
+        }
+
+        if (IsMovingDown)
+        {
+            transform.Translate(-Vector3.right * MovementSpeed * Time.deltaTime);
+        }
+	}
+
+    private void OnTriggerEnter(Collider other)
+    {
+        IsMovingRight = false;
+        IsMovingLeft = false;
+        IsMovingDown = false;
+        IsMovingUp = false;
+        transform.Translate(new Vector3(0, 0, 0));
+
+        if (this.tag == "Key")
+        {
+            if (other.tag == "WinBorder")
+            {
+                // The Game is Won
+                Debug.Log("Game is WON");
+            }
+        }
+    }
+}
