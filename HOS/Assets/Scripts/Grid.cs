42b1a26e4e9aedb24ad42829bbb8243379cd3d08
Schamale <38950754+Schamale@users.noreply.github.com>
2019-03-11 14:47:32 -0400

    Adding Grid and Node Scripts for A*
    Adding Scripts for A* fixing buggs with movement of mouse mini game.

---
+++ HOS/Assets/Scripts/Grid.cs
@@ -0,0 +1,61 @@
+﻿using UnityEngine;
+using System.Collections;
+using HOS;
+
+public class Grid : MonoBehaviour {
+    #region Member Variables
+    public LayerMask unwalkableMask;        //Layer used for objectes meant to be unwalkable
+	public Vector2 gridWorldSize;           //Create The grid size in the Inspector
+	public float nodeRadius;                //Creates Radius for grid in the inspector
+	public Node[,] grid;                    //2D array for the Node class
+
+	float nodeDiameter;
+	int gridSizeX, gridSizeY;
+    #endregion
+
+    #region Default Constructor
+    void Start() {
+		nodeDiameter = nodeRadius*2;
+		gridSizeX = Mathf.RoundToInt(gridWorldSize.x/nodeDiameter);
+		gridSizeY = Mathf.RoundToInt(gridWorldSize.y/nodeDiameter);
+		CreateGrid();
+	}
+    #endregion
+    #region Grid Constructor 
+    void CreateGrid() {
+		grid = new Node[gridSizeX,gridSizeY];
+		Vector3 worldBottomLeft = transform.position - Vector3.right * gridWorldSize.x/2 - Vector3.forward * gridWorldSize.y/2;
+
+		for (int x = 0; x < gridSizeX; x ++) {
+			for (int y = 0; y < gridSizeY; y ++) {
+				Vector3 worldPoint = worldBottomLeft + Vector3.right * (x * nodeDiameter + nodeRadius) + Vector3.forward * (y * nodeDiameter + nodeRadius);
+				bool walkable = !(Physics.CheckSphere(worldPoint,nodeRadius,unwalkableMask));
+				grid[x,y] = new Node(walkable,worldPoint);
+			}
+		}
+	}
+    #endregion
+    public Node NodeFromWorldPoint(Vector3 worldPosition) {
+		float percentX = (worldPosition.x + gridWorldSize.x/2) / gridWorldSize.x;
+		float percentY = (worldPosition.z + gridWorldSize.y/2) / gridWorldSize.y;
+		percentX = Mathf.Clamp01(percentX);
+		percentY = Mathf.Clamp01(percentY);
+
+		int x = Mathf.RoundToInt((gridSizeX-1) * percentX);
+		int y = Mathf.RoundToInt((gridSizeY-1) * percentY);
+		return grid[x,y];
+	}
+
+	void OnDrawGizmos() {
+        Gizmos.color = Color.blue;
+		Gizmos.DrawWireCube(transform.position,new Vector3(gridWorldSize.x,1,gridWorldSize.y));
+
+	
+		if (grid != null) {
+			foreach (Node n in grid) {
+				Gizmos.color = (n.walkable)?Color.green:Color.red;
+				Gizmos.DrawCube(n.worldPosition, Vector3.one * (nodeDiameter-.1f));
+			}
+		}
+	}
+}
---
+++ HOS/Assets/Scripts/Node.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using HOS;
+
+public class Node
+{
+    #region Member Variables
+    public bool walkable;
+    public Vector3 worldPosition;
+
+    #region Default Constructor
+    public Node (bool _walkable, Vector3 _worldPos)
+    {
+        walkable = _walkable;
+        worldPosition = _worldPos;
+    }
+    #endregion
+
+    #endregion
+
+}
+
