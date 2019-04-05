65bcda2c2abc3ede2ecf90c4722dd2d85451dc46
Laurel Tyndall <laureltyndall@yahoo.com>
2019-04-05 16:55:42 -0400

    Fountain Puzzle Winable. Just need to add rainbow help panel.

--- HOS/Assets/Scripts/FountainPuzzleController.cs
+++ HOS/Assets/Scripts/FountainPuzzleController.cs
@@ -14,10 +14,13 @@ public class FountainPuzzleController : MonoBehaviour {
     public GameObject StarRiddleSphere;
     public GameObject RainbowRiddleSphere;
     public GameObject CloseUpCamera;
-    private float StarTurnTimer = 4f;
+    public float StarTurnTimer = 4f;
     private bool BeginStars = false;
     public bool MoonstoneInsterted = false;
     private bool RiddleSolved = false;
+    public int ClickNumber = 0;
+    public int NumberCorrect = 0;
+    private bool GameOver = false;
 
     private int WrongClicksCount = 0;
 
@@ -30,49 +33,144 @@ public class FountainPuzzleController : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
-        if (!MoonstoneInsterted)
+        if (!GameOver)
         {
-            CloseUpCamera.SetActive(false);
-        }
-        else
-        {
-            if (RiddleSolved)
+            if (!MoonstoneInsterted)
             {
-                if (BeginStars)
+                CloseUpCamera.SetActive(false);
+            }
+            else
+            {
+                if (RiddleSolved)
                 {
-                    if (StarTurnTimer > 0f)
+                    if (BeginStars)
                     {
-                        TextAreaSmall.text = "It looks like the stars are turning around.";
-
-                        StarTurnTimer -= Time.deltaTime;
-
-                        foreach (GameObject go in StarLocations)
+                        if (StarTurnTimer > 0f)
                         {
-                            go.gameObject.GetComponent<RotateStars>().StarsTurning = true;
+                            TextAreaSmall.text = "It looks like the stars are turning around.";
+
+                            StarTurnTimer -= Time.deltaTime;
+
+                            foreach (GameObject go in StarLocations)
+                            {
+                                go.gameObject.GetComponent<RotateStars>().StarsTurning = true;
+                            }
+                        }
+                        else
+                        {
+                            foreach (GameObject go in ClickableStars)
+                            {
+                                go.transform.position = new Vector3(go.transform.position.x, 0.047f, go.transform.position.z);
+                                go.gameObject.GetComponent<StarIndicator>().Clickable = true;
+                            }
+
+                            BeginStars = false;
                         }
                     }
                     else
                     {
-                        //foreach (GameObject go in StarLocations)
-                        //{
-                        //    go.SetActive(false);
-                        //}
                         foreach (GameObject go in ClickableStars)
                         {
-                            go.transform.position = new Vector3(go.transform.position.x, 0.047f, go.transform.position.z);
+                            if (go.gameObject.GetComponent<StarIndicator>().Clicked)
+                            {
+                                if (go.gameObject.GetComponent<StarIndicator>().ClickOrder == 0)
+                                {
+                                    ClickNumber++;
+                                    go.gameObject.GetComponent<StarIndicator>().ClickOrder = ClickNumber;
+                                }
+                            }
                         }
 
-                        BeginStars = false;
+                        if (ClickNumber == 7)
+                        {
+                            foreach (GameObject go in ClickableStars)
+                            {
+                                if (go.gameObject.GetComponent<StarIndicator>().Clicked)
+                                {
+                                    if (go.gameObject.GetComponent<StarIndicator>().ClickOrder == 1)
+                                    {
+                                        if (go.name == "Violet Star")
+                                        {
+                                            NumberCorrect++;
+                                        }
+                                    }
+                                    else if (go.gameObject.GetComponent<StarIndicator>().ClickOrder == 2)
+                                    {
+                                        if (go.name == "Indigo Star")
+                                        {
+                                            NumberCorrect++;
+                                        }
+                                    }
+                                    else if (go.gameObject.GetComponent<StarIndicator>().ClickOrder == 3)
+                                    {
+                                        if (go.name == "Blue Star")
+                                        {
+                                            NumberCorrect++;
+                                        }
+                                    }
+                                    else if (go.gameObject.GetComponent<StarIndicator>().ClickOrder == 4)
+                                    {
+                                        if (go.name == "Green Star")
+                                        {
+                                            NumberCorrect++;
+                                        }
+                                    }
+                                    else if (go.gameObject.GetComponent<StarIndicator>().ClickOrder == 5)
+                                    {
+                                        if (go.name == "Yellow Star")
+                                        {
+                                            NumberCorrect++;
+                                        }
+                                    }
+                                    else if (go.gameObject.GetComponent<StarIndicator>().ClickOrder == 6)
+                                    {
+                                        if (go.name == "Orange Star")
+                                        {
+                                            NumberCorrect++;
+                                        }
+                                    }
+                                    else if (go.gameObject.GetComponent<StarIndicator>().ClickOrder == 7)
+                                    {
+                                        if (go.name == "Red Star")
+                                        {
+                                            NumberCorrect++;
+                                        }
+                                    }
+                                }
+                            }
+
+                            if (NumberCorrect == 7)
+                            {
+                                TextAreaSmall.text = "Got it!";
+                                GameOver = true;
+                            }
+                            else
+                            {
+                                TextAreaSmall.text = "I don't think that was right. \n I need to click on the stars in the order of the colors in a rainbow.";
+                                foreach (GameObject go in ClickableStars)
+                                {
+                                    go.gameObject.GetComponent<StarIndicator>().Clicked = false;
+                                    go.gameObject.GetComponent<StarIndicator>().ClickOrder = 0;
+                                    NumberCorrect = 0;
+                                    ClickNumber = 0;
+                                }
+                            }
+                        }
                     }
                 }
+                else
+                {
+                    CloseUpCamera.SetActive(true);
+                    RiddleAnswerPanel.SetActive(true);
+                    StarRiddleSphere.SetActive(false);
+                    MoonstoneStarEmpty.SetActive(false);
+                    TextAreaSmall.text = "'You see me in the air, but I am not a kite. I am what's created when water refracts light.' - Hm.";
+                }
             }
-            else
-            {
-                CloseUpCamera.SetActive(true);
-                RiddleAnswerPanel.SetActive(true);
-                StarRiddleSphere.SetActive(false);
-                TextAreaSmall.text = "'You see me in the air, but I am not a kite. I am what's created when water refracts light.' - Hm.";
-            }
+        }
+        else
+        {
+
         }
     }
 
