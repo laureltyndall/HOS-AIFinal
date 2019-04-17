760b6202eee2cf3f80a5fbb68bf6dbb044e1c606
Schamale <38950754+Schamale@users.noreply.github.com>
2019-04-16 20:05:51 -0400

    animators and Wolf Target AI changes.

---
+++ HOS/Assets/Scripts/WolfBaseFSM.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using HOS;
+
+public class WolfBaseFSM : StateMachineBehaviour
+{
+
+    public GameObject WolfNPC;
+    public GameObject WolfOppenent;
+    public float speed = 2.0f;
+    public float rotSpeed = 1.0f;
+    public float accuracy = 3.0f;
+
+    public override void OnStateEnter(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
+    {
+        WolfNPC = animator.gameObject;
+        WolfOppenent = WolfNPC.GetComponent<WolfAI>().GetTarget();
+    }
+}
